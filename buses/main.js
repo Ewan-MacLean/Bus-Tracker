8db0a6f5603d02ae1ea316150db8d3d4dfe24930
Ewan MacLean <git config --global user.email ewanmaclean77@gmail.comgit config --global user.email>
2022-05-01 23:34:55 -0300

    updated

--- buses/main.js
+++ buses/main.js
@@ -1,7 +1,9 @@
 (function(){
 
     let busIcon = L.icon({iconUrl: 'bus.png', iconSize: [30,40]});
+    // set the map coordinates
     let map = L.map('theMap').setView([44.650627,-63.597140], 14);
+    // apply bus locations to the bus elements
     busLayer = L.geoJSON(null,{pointToLayer: function (feature, latlng) {
 
         //adding the bus info popup (OLD VERSION)
@@ -16,6 +18,11 @@
         //     .openOn(map)
         // }  
 
+        // for each element, return a bus Icon with bearing specific to the 
+        // location data passed up from generateGeoFromLatLong.
+        // then add location data to the popup that appears upon
+        // clicking the bus.
+        // ...Then add it to the map
         return L.marker(latlng,
             {
                 icon: busIcon,
@@ -28,18 +35,20 @@
             
     }}).addTo(map);
 
-    
+    // set the map display
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
 
-    // show initial map, then update the locations of the buses
+    // show initial map with buses
     moment(map)
-    setInterval(moment,8000,map);
+    // upate locations every 8 seconds
+    setInterval(moment,3000,map);
 
 })();
 
+    // generate bus location data
 function generateGeoFromLatLong(lat,long,bearing,routeID)
 {
     return ({
@@ -56,12 +65,14 @@ function generateGeoFromLatLong(lat,long,bearing,routeID)
 
 };
 
-
+// this function retrieves the live data for the bus locations
 function moment(map) {
     fetch('http://hrmbusapi.herokuapp.com/')
     .then(res=>res.json())
     .then(json=>{
         busLayer.clearLayers();
+        // for each element in the routeID array. Create these 4 variables and assign them to
+        // the array element. Then add that data to the bus layer
         json.entity.filter(x=>x.vehicle.trip.routeId < 11).map(el=>{
             const lat = el.vehicle.position.latitude;
             const long = el.vehicle.position.longitude;
